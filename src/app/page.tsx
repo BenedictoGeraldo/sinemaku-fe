b1a6cc96a481a4b68494df74cbffb36e0ad59ddb
BenedictoGeraldo <geraldodawa@gmail.com>
2026-03-14 22:04:34 +0700

    update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/navbar";
 import TrendingView from "@/views/landing/trending";
 import TopRatedView from "@/views/landing/topRated";
 import PopularView from "@/views/landing/popular";
@@ -5,6 +6,7 @@ import PopularView from "@/views/landing/popular";
 export default function HomePage() {
   return (
     <>
+      <Navbar />
       <TrendingView />
       <TopRatedView />
       <PopularView />
